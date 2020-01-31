e882511033a4e60f93915d52c1540c57bae0c0f7
victoriabrandon <joshuach1.9@gmail.com>
2020-01-30 20:40:38 -0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,49 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double itemPrice = 43.50;
+int itemPriceTwo = 63;
+System.out.println(itemPrice);
+System.out.println(itemPriceTwo);
+
+double amtMoneyWallet = 100.00;
+double amtMoneyInWallet = 125.78;
+System.out.println(amtMoneyWallet);
+System.out.println(amtMoneyInWallet);
+
+int numberOfFriends = 56;
+int numberOfFriendsII = 15;
+System.out.println(numberOfFriends);
+System.out.println(numberOfFriendsII);
+
+int ageInYears = 76;
+int ageInYearsII = 114;
+System.out.println(ageInYears);
+System.out.println(ageInYearsII);
+
+String firstName = "Susie";
+String firstNameII = "Johnny";
+System.out.println(firstName);
+System.out.println(firstNameII);
+
+String lastName = "Miller";
+String lastNameII = "Lennon";
+System.out.println(lastName);
+System.out.println(lastNameII);
+
+char middleInitial = 'C';
+char middleInitialII = 'A';
+System.out.println(middleInitial);
+System.out.println(middleInitialII);
+
+
+
+
+
+
+
+	}
+
+}
---
+++ src/App2.java
@@ -0,0 +1,25 @@
+
+public class App2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double walletBalance = 152.05;
+System.out.println(walletBalance);
+walletBalance = walletBalance - 5.42;
+System.out.println(walletBalance);
+
+int age =75 * 3;
+System.out.println(age);
+int numberOfFriends = 225 + 451;
+System.out.println(numberOfFriends);
+
+String firstName = "Johanna";
+String lastName = "Marks";
+String fullName = firstName + " " + "C"+ " " + lastName;
+System.out.println(fullName);
+
+String myFavoriteColor = "Yellow";
+System.out.println("My Favorite Color is:" + myFavoriteColor);
+
+}
+}
